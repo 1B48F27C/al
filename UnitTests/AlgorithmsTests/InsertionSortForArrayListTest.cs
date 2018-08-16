9c80e6523d14b0609145f64592fcc0ef1f2715bb
IvanM <ivan.mkhitaryan@adacta-group.com>
2018-08-16 09:32:22 +0300

    adding new test for IList

---
+++ UnitTests/AlgorithmsTests/InsertionSortForArrayListTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using DataStructures.Lists;
+using Algorithms.Sorting;
+using Xunit;
+
+namespace UnitTest.AlgorithmsTests
+{
+    public static class InsertionSortForArrayListTest
+    {
+        [Fact]
+        public static void DoTest()
+        {
+            var list1 = new ArrayList<int>
+            {
+                23,
+                42,
+                4,
+                16,
+                8,
+                -8,
+                9,
+                55,
+                0,
+                34,
+                12,
+                2,
+                -10,
+                -98
+            };
+
+            list1.InsertionSort<int>();
+            bool isListSorted = true;
+            for (int i = 0; i < list1.Count - 1; i++)
+            {
+                if (list1[i] > list1[i+1])
+                {
+                    isListSorted = false;
+                    break;
+                }
+            }
+
+            Assert.True(isListSorted);
+        }
+    }
+}
+
